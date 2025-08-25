3ceed143a25e5a4ea0bf95631b9bf38251102104
Pavani Tatiparti <152892161+pavanitatiparti7@users.noreply.github.com>
2025-08-25 10:19:56 +0530

    Merge Strings Alternatively

--- 1768.java
+++ 1768.java
@@ -20,21 +20,21 @@ class Solution {
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         int n = Math.min(word1.length(), word2.length());
-        StringBuilder sb = new StringBuilder();
+        StringBuilder res = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
+            res.append(word1.charAt(i));
+            res.append(word2.charAt(i));
         }
 
         if (word1.length() > n) {
-            sb.append(word1.substring(n));
+            res.append(word1.substring(n));
         }
         if (word2.length() > n) {
-            sb.append(word2.substring(n));
+            res.append(word2.substring(n));
         }
 
-        return sb.toString();
+        return res.toString();
     }
 }
 
