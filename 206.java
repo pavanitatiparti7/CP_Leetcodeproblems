f9a3b6031ccc02de7f6fbcef2f1278e05160002d
Pavani Tatiparti <152892161+pavanitatiparti7@users.noreply.github.com>
2025-12-08 12:08:27 +0530

    Reverse Linked List

--- 206.java
+++ 206.java
@@ -10,16 +10,34 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next;  // store next node
-            curr.next = prev;           // reverse link
-            prev = curr;                // move prev forward
-            curr = next;                // move curr forward
+        Stack<Integer> s=new Stack<>();
+        while(head != null){
+            s.push(head.val);
+            head=head.next;
         }
-
-        return prev; // new head
+        ListNode dummy=new ListNode(99);
+        ListNode res=dummy;
+        while(!s.isEmpty()){
+            ListNode ptr=new ListNode(s.pop());
+            dummy.next=ptr;
+            dummy=dummy.next;
+        }
+        return res.next;
     }
-}
+ }
+//OR
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ListNode prev = null;
+//         ListNode curr = head;
+
+//         while (curr != null) {
+//             ListNode next = curr.next;  // store next node
+//             curr.next = prev;           // reverse link
+//             prev = curr;                // move prev forward
+//             curr = next;                // move curr forward
+//         }
+
+//         return prev; // new head
+//     }
+// }
