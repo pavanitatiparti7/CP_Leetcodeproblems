9dcbfb03f104b56e404851a1a9fc3b5e70599b8b
Pavani Tatiparti <152892161+pavanitatiparti7@users.noreply.github.com>
2025-12-31 10:15:54 +0530

    Binary Tree Right Side View
    Refactor rightSideView method to improve clarity and efficiency.

--- 199.java
+++ 199.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res=new ArrayList<>();
@@ -42,3 +43,33 @@ class Solution {
         return res;
     }
 }
+
+//Without lastval
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> res=new ArrayList<>();
+//         if(root==null){
+//             return res;
+//         }
+//         Queue<TreeNode> queue=new LinkedList<>();
+
+//         queue.add(root);
+
+//         while(!queue.isEmpty()){
+//             int size=queue.size();
+//             TreeNode node=null;
+//             for(int i=0;i<size;i++){
+//                 node=queue.poll();
+//                 if(node.left!=null){
+//                     queue.add(node.left);
+//                 }
+//                 if(node.right!=null){
+//                     queue.add(node.right);
+     
+//                 }
+//             }
+//             res.add(node.val);
+//         }
+//         return res;
+//     }
+// }
