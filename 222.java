7dd353ed32918d5034883062c45a4c75a34b1378
Pavani Tatiparti <152892161+pavanitatiparti7@users.noreply.github.com>
2026-01-07 12:13:33 +0530

    Count Complete Tree Nodes

--- 222.java
+++ 222.java
@@ -13,26 +13,63 @@
  *     }
  * }
  */
+
 class Solution {
     public int countNodes(TreeNode root) {
-        
-        Queue<TreeNode> queue=new LinkedList<>();
-        int cnt=0;
-        if(root==null){
-            return cnt;
+        int leftdep=leftdepth(root);
+        int rightdep= rightdepth(root);
+        if(leftdep==rightdep){
+            return (int)Math.pow(2, leftdep)-1;
+        }else{
+            return 1+countNodes(root.left)+countNodes(root.right);
         }
-        queue.add(root);
-
-        while(!queue.isEmpty()){
-            TreeNode node=queue.poll();
-            cnt++;
-            if(node.left!=null){
-                queue.add(node.left);
-            }
-            if(node.right!=null){
-                queue.add(node.right);
-            }
+    }
+    public int leftdepth(TreeNode root){
+        int dep=0;
+        while(root!=null){
+            dep++;
+            root=root.left;
         }
-        return cnt;
+        return dep;
+    }
+    public int rightdepth(TreeNode root){
+        int dep=0;
+        while(root!=null){
+            dep++;
+            root=root.right;
+        }
+        return dep;
     }
 }
+
+
+
+
+// class Solution {
+//     public int countNodes(TreeNode root) {        
+//         Queue<TreeNode> queue=new LinkedList<>();
+//         int cnt=0;
+//         // if(root==null){
+//         //     return cnt;
+//         // }
+//         //OR 0
+//         if(root==null){
+//             return 0;
+//         }
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             TreeNode node=queue.poll();
+//             cnt++;
+//             if(node.left!=null){
+//                 queue.add(node.left);
+//             }
+//             if(node.right!=null){
+//                 queue.add(node.right);
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
+
+
